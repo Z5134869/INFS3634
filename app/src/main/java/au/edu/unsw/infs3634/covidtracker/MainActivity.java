0b4b756344bbead02af8565c435d4030d979e64a
luzejia Z5134869 <zejia.lu@student.unsw.edu.au>
2020-09-26 12:14:04 +0800

    successfully install youtube url in.

--- app/src/main/java/au/edu/unsw/infs3634/covidtracker/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/covidtracker/MainActivity.java
@@ -1,6 +1,8 @@
 package au.edu.unsw.infs3634.covidtracker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +15,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.btnLaunchActivity);
-        button.setText("Text changes");
+        //button.setText("Text changes");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchDetailActivity("Test: Message from MainActivity");
+                //Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                //intent.putExtra(DetailActivity.INTENT_MESSAGE,  "Test: Message from MainActivity");
+                //startActivity(intent);
+            }
+        });
+    }
+    private void launchDetailActivity(String message){
+        Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+        intent.putExtra(DetailActivity.INTENT_MESSAGE,  message);
+        startActivity(intent);
     }
 }
