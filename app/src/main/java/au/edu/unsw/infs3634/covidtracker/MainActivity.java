b02dfbdb75b6850763f9153092104b5ac4948179
luzejia Z5134869 <zejia.lu@student.unsw.edu.au>
2020-10-08 20:31:21 +0800

    changes in MainActivity

--- app/src/main/java/au/edu/unsw/infs3634/covidtracker/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/covidtracker/MainActivity.java
@@ -1,28 +1,27 @@
 package au.edu.unsw.infs3634.covidtracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+private RecyclerView mRecyclerView;
+private CountryAdapter mAdapter;
+private RecyclerView.LayoutManager LayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button = findViewById(R.id.btLaunchActivity);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                launchDetailActivity("US");
-            }
-        });
-    }
+        mRecyclerView = findViewById(R.id.rvList);
+        mRecyclerView.setHasFixedSize(true);
+        LayoutManager = new LinearLayoutManager(this);
+        mAdapter = new CountryAdapter(Country.getCountries());
+        mRecyclerView.setAdapter(mAdapter);
+    } 
 
     private void launchDetailActivity(String message) {
         Intent intent = new Intent(this, DetailActivity.class);
