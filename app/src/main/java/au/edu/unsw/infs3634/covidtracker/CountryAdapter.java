b1694d8835643c633497003d1dcde5ffa6a5f95d
luzejia Z5134869 <zejia.lu@student.unsw.edu.au>
2020-10-13 16:29:22 +0800

    week5 half of work, avoid no battery of mac, and cant commit

--- app/src/main/java/au/edu/unsw/infs3634/covidtracker/CountryAdapter.java
+++ app/src/main/java/au/edu/unsw/infs3634/covidtracker/CountryAdapter.java
@@ -3,6 +3,8 @@ package au.edu.unsw.infs3634.covidtracker;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -10,20 +12,58 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
-public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.CountryViewHolder> {
+public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.CountryViewHolder>  implements Filterable {
     private ArrayList<Country> mCountries;
     private RecyclerViewClickListener mListener;
+    private ArrayList<Country> mCountriesFiltered;
 
     public CountryAdapter(ArrayList<Country> countries, RecyclerViewClickListener listener) {
         mCountries = countries;
         mListener = listener;
+        mCountriesFiltered = countries;
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                String searchQuery = constraint.toString();
+                if(searchQuery.isEmpty()){
+                    mCountriesFiltered = mCountries;
+                }else {
+                    ArrayList<Country> filterList = new ArrayList<>();
+                    for(Country country : mCountries){
+                        if(country.getCountry().toLowerCase().contains(searchQuery.toLowerCase())){
+                            filterList.add(country);
+                        }
+                    }
+                    mCountriesFiltered = filterList;
+                }
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = mCountriesFiltered;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                mCountriesFiltered = (ArrayList<Country>) results.values;
+                notifyDataSetChanged();
+
+            }
+        };
     }
 
     public interface RecyclerViewClickListener {
         void onClick(View view, String countryCode);
     }
 
+    public interface Listener{
+        void onClick(View view, String countryCode);
+    }
     @NonNull
     @Override
     public CountryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -43,7 +83,7 @@ public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.CountryV
 
     @Override
     public int getItemCount() {
-        return mCountries.size();
+        return mCountriesFiltered.size();
     }
 
     public static class CountryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
@@ -62,6 +102,21 @@ public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.CountryV
         @Override
         public void onClick(View v) {
             listener.onClick(v, (String) v.getTag());
+        }
+
+        public void sort(final int sortMethod){
+            if (mCountriesFiltered.size() > 0){
+                Collections.sort(mCountriesFiltered, new Comparator<Country>() {
+                    @Override
+                    public int compare(Country o1, Country o2){
+                        if(sortMethod == 1){
+                            return o2.getNewConfirmed().compareTo(o1.getTotalConfirmed());
+                        }
+                        return o2.getTotalConfirmed().compareTo(o1.getTotalConfirmed());
+                    }
+                });
+            }
+            notifyDataSetChanged();
         }
     }
 }
--- app/src/main/java/au/edu/unsw/infs3634/covidtracker/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/covidtracker/MainActivity.java
@@ -1,13 +1,15 @@
 package au.edu.unsw.infs3634.covidtracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.SearchView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
@@ -34,5 +36,38 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, DetailActivity.class);
         intent.putExtra(DetailActivity.INTENT_MESSAGE, message);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        SearchView searchView = (SearchView) menu.findItem(R.id.app_bar_).getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+            @Override
+            public boolean onQueryTextSubmit(String query){
+                mAdapter.getFilter().filter(query);
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText){
+                mAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.sort_new:
+                mAdapter.sort(1);
+                return true;
+            case R.id.sort_total:
+                mAdapter.sort(2);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
     }
 }
